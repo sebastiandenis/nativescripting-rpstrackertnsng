bc0ed1ef4afeef2494dbb57456176cb7ce9ae292
Sebastian Denis <sebastian.denis@outlook.com>
2019-01-28 09:18:19 +0100

    Lesson: Error Handling

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,19 +1,28 @@
-import { NgModule, Optional, SkipSelf } from "@angular/core";
+import { NgModule, Optional, SkipSelf, ErrorHandler } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AuthService, LoggerService } from "./services";
-import {  } from "./services/logger.service";
+import {} from "./services/logger.service";
 import { SERVICES } from "./services";
+import { RpsErrorHandler } from "./helpers";
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
   exports: [],
-   providers: [...SERVICES]
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: RpsErrorHandler
+    },
+    ...SERVICES
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
-      throw new Error("CoreModule has already been loaded. Import CoreModule into the AppModule only.");
+      throw new Error(
+        "CoreModule has already been loaded. Import CoreModule into the AppModule only."
+      );
     }
   }
 }
